0209c366c6683172abb97d7a2acb5c54b799ff94
kaustubh1412 <kaustubh.commercial,1999@gmail.com>
2020-10-29 14:57:44 +0530

    added frontend files

---
+++ script.js
@@ -0,0 +1,28 @@
+// updated 2019
+const input = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+
+const expand = () => {
+  searchBtn.classList.toggle("close");
+  input.classList.toggle("square");
+};
+
+searchBtn.addEventListener("click", expand);
+
+
+window.onscroll = function() {scrollFunction()};
+
+
+//  old version / jquery
+//
+// function expand() {
+//   $(".search").toggleClass("close");
+//   $(".input").toggleClass("square");
+//   if ($('.search').hasClass('close')) {
+//     $('input').focus();
+//   } else {
+//     $('input').blur();
+//   }
+// }
+// $('button').on('click', expand);
+//
